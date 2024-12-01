b264ccfd0adfc3e54d93f83b6cb64993b5b778ea
Nahida <mailagain.hurt@gmail.com>
2024-12-01 16:18:46 +0800

    Site updated: 2024-12-01 16:18:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/01/hello-world/"];function toRandomPost(){
+var posts=["2024/12/01/这是第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
